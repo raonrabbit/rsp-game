4652b13e63a63e9e489128b9055fa00d0466f0ce
raonrabbit <raonrabbit@gmail.com>
2023-12-27 22:01:31 +0900

    fix: 음향세팅 저장 안되는 문제 수정

--- Sound/soundSetting.cs
+++ Sound/soundSetting.cs
@@ -10,31 +10,48 @@ public class soundSetting : MonoBehaviour
     public Slider BGMAudioSlider;
     public Slider SFXAudioSlider;
     public Slider MobAudioSlider;
+    void Awake(){
+        if(PlayerPrefs.HasKey("MasterVolume"))
+            MasterAudioSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+        if(PlayerPrefs.HasKey("BGMVolume"))
+            BGMAudioSlider.value = PlayerPrefs.GetFloat("BGMVolume");
+        if(PlayerPrefs.HasKey("SFXVolume"))
+            SFXAudioSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        if(PlayerPrefs.HasKey("MobVolume"))
+            MobAudioSlider.value = PlayerPrefs.GetFloat("MobVolume");
+    }
 
+    void Start(){
+        masterAudioControl();
+        BGMAudioControl();
+        SFXAudioControl();
+        MobAudioControl();
+    }
+    
     public void masterAudioControl(){
         float sound = MasterAudioSlider.value;
-
+        PlayerPrefs.SetFloat("MasterVolume", sound);
         if(sound == -40f) masterMixer.SetFloat("MasterVolume", -80);
         else masterMixer.SetFloat("MasterVolume", sound);
     }
 
     public void BGMAudioControl(){
         float sound = BGMAudioSlider.value;
-
+        PlayerPrefs.SetFloat("BGMVolume", sound);
         if(sound == -40f) masterMixer.SetFloat("BGMVolume", -80);
         else masterMixer.SetFloat("BGMVolume", sound);
     }
 
     public void SFXAudioControl(){
         float sound = SFXAudioSlider.value;
-
+        PlayerPrefs.SetFloat("SFXVolume", sound);
         if(sound == -40f) masterMixer.SetFloat("SFXVolume", -80);
         else masterMixer.SetFloat("SFXVolume", sound);
     }
 
     public void MobAudioControl(){
         float sound = MobAudioSlider.value;
-
+        PlayerPrefs.SetFloat("MobVolume", sound);
         if(sound == -40f) masterMixer.SetFloat("MobVolume", -80);
         else masterMixer.SetFloat("MobVolume", sound);
     }
